c968117003f7e3e4737795c0c4f7de9be579882a
Sampreeti Kar <sampreeti092@gmail.com>
2026-02-21 16:42:41 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://username.github.io',
-  base: '/repository-name/'
+  site: 'https://MarshX9.github.io',
+  base: '/ITERON/'
 });
